8592fe0f20e2ccb86ef094a5a1710d55289086bd
whynotwhywhywhywhy <naomi981104@gmail.com>
2023-03-08 23:26:42 +0900

    day 2

--- 220308.js
+++ 220308.js
@@ -13,3 +13,15 @@ function solution(n) {
 
   return pizzaBox / 6;
 }
+
+// 고작 이거 커밋하다가 깃허브에서 만든 리드미 파일때문에 병합이 안됨
+// 해결 방법 - 1
+// git push origin main --force
+// --force를 쓰면 이전 커밋 내용이 날라간다.
+
+// 해결 방법 - 2
+// git checkout master
+// git pull origin master
+// git checkout 내가 작업한 브랜치
+// git merge master
+// 브랜치 두개의 내용을 pull로 합쳐준다.
