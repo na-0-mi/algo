c257553b2fa9e2f32f192b3bc7102b0c7be11d0c
whynotwhywhywhywhy <naomi981104@gmail.com>
2023-03-10 17:15:11 +0900

    day 4

--- 230309.js
+++ 230309.js
@@ -11,3 +11,13 @@ function solution(numbers) {
 }
 
 // 옷가게 할인 받기
+function solution(price) {
+  if (price >= 500000) {
+    price *= 0.8;
+  } else if (price >= 300000) {
+    price *= 0.9;
+  } else if (price >= 100000) {
+    price *= 0.95;
+  }
+  return Math.floor(price);
+}
